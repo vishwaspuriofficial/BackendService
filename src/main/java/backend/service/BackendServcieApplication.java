5a0ba82ea69ea47ff96e7bb6ba72f9d38f382767
Vishwas Puri <vishwaspuriofficial@gmail.com>
2024-07-14 20:04:29 -0400

    initial commit - spring boot setup

---
+++ src/main/java/backend/service/BackendServcieApplication.java
@@ -0,0 +1,13 @@
+package backend.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendServcieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendServcieApplication.class, args);
+	}
+
+}
