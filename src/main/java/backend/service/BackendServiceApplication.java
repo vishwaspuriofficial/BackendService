9a31312afca90d23cd62b9869d857b9e0aed7982
Vishwas Puri <vishwaspuriofficial@gmail.com>
2024-07-14 21:01:23 -0400

    Fixed naming error

--- src/main/java/backend/service/BackendServiceApplication.java
+++ src/main/java/backend/service/BackendServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendServcieApplication {
+public class BackendServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendServcieApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendServiceApplication.class, args);
+    }
 
 }
